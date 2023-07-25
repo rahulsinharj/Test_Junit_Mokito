2c163e8854b1d3c2ddf6b1fa857c3c15b82b2342
rahulsinharj <rahulkumarsinha1234@gmail.com>
2023-07-25 21:29:37 +0530

    adding new test proj

---
+++ JunitTEst/src/test/java/helper/ArrayCompareTest.java
@@ -0,0 +1,34 @@
+package helper;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class ArrayCompareTest {
+
+    @Test
+    public void testArraySort() {
+        int[] number = {12, 3, 4, 1};
+        int[] expected = {1, 3, 4, 12};                 // {1, 4, 3, 12};
+        Arrays.sort(number);
+
+        assertArrayEquals(expected, number);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testArraySort_NullArray(){
+        int[] number = {};                            // int[] number = {} ;
+        Arrays.sort(number);
+
+    }
+
+    @Test()
+    public void testArray(){
+        int[] number = {};                            // int[] number = {} ;
+        Arrays.sort(number);
+
+    }
+
+}
