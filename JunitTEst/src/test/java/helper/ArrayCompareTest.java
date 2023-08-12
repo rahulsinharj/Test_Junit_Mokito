ca5e1ebe7ec4cf8f2759621b94b92616cbd50d38
rahulsinharj <rahulkumarsinha1234@gmail.com>
2023-08-12 17:59:13 +0530

    StringHelperParameterizedTest

--- JunitTEst/src/test/java/helper/ArrayCompareTest.java
+++ JunitTEst/src/test/java/helper/ArrayCompareTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
 
 public class ArrayCompareTest {
 
+    // Comparing Arrays in Junit test :
     @Test
     public void testArraySort() {
         int[] number = {12, 3, 4, 1};
@@ -17,6 +18,7 @@ public class ArrayCompareTest {
         assertArrayEquals(expected, number);
     }
 
+    // Testing Exception in Junit test :
     @Test(expected = NullPointerException.class)
     public void testArraySort_NullArray(){
         int[] number = null;                            // int[] number = {} ;
@@ -24,11 +26,14 @@ public class ArrayCompareTest {
 
     }
 
-/*    @Test()
-    public void testArray(){
-        int[] number = {};                            // int[] number = {} ;
-        Arrays.sort(number);
-
-    }*/
+    // Testing Performance in Junit test :
+    @Test(timeout = 100)
+    public void testArraySort_Performance(){
+        int[] arr = {12, 23, 4};
+        for(int i=0; i<10000; i++){
+            arr[0] = i;
+            Arrays.sort(arr);
+        }
+    }
 
 }
